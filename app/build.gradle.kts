507d32546d3520015bb610c0af92612f17be5d25
CodeStarter40 <bhgzpro@pm.me>
2024-03-12 23:36:09 +0100

    mise à jour Gradle en vue d'une mise en place d'un ViewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.openclassrooms.notes"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.openclassrooms.notes"
@@ -39,14 +39,25 @@ android {
 }
 
 dependencies {
+    val lifecycle_version = "2.7.0"
+    val arch_version = "2.2.0"
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version") //viewmodel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycle_version") //Viewmodel utilities for Compose
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version") //LiveData
+    implementation("androidx.lifecycle:lifecycle-common-java8:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:$lifecycle_version") //saved state module for viewmodel
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_version") //lifeCycle Only without View Midel or livedata
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.recyclerview:recyclerview:1.3.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("androidx.arch.core:core-testing:$arch_version") // test helpers for livedata
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
 }
